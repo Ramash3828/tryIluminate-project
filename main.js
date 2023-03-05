da5e1c51555d119b54aadcc6774731fe5ced87fa
Ramash Halder <ramash3828@gmail.com>
2023-03-05 10:29:12 +0600

    video section done

--- main.js
+++ main.js
@@ -64,3 +64,37 @@ $(".all-collections__slider").slick({
 		},
 	],
 });
+
+// Video slider----------
+$(".video__content").slick({
+	dots: false,
+	infinite: false,
+	speed: 300,
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	responsive: [
+		{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 4,
+				slidesToScroll: 1,
+				infinite: true,
+				dots: false,
+			},
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+			},
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 2.5,
+				slidesToScroll: 1,
+			},
+		},
+	],
+});
