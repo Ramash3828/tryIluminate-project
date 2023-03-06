d821c58c24fa6629f799d4a609689eca3f651f9a
Ramash Halder <ramash3828@gmail.com>
2023-03-06 14:59:57 +0600

    announcement var done

--- main.js
+++ main.js
@@ -42,54 +42,54 @@ function runSlider() {
 				},
 			],
 		});
+
+		// Allcollection Slider
+		$(".all-collections__slider").slick({
+			dots: false,
+			infinite: false,
+			speed: 300,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			responsive: [
+				{
+					breakpoint: 1024,
+					settings: {
+						slidesToShow: 2.25,
+						slidesToScroll: 1,
+						infinite: true,
+						dots: false,
+					},
+				},
+				{
+					breakpoint: 600,
+					settings: {
+						slidesToShow: 2.25,
+						slidesToScroll: 1,
+					},
+				},
+				{
+					breakpoint: 480,
+					settings: {
+						slidesToShow: 2.25,
+						slidesToScroll: 1,
+					},
+				},
+			],
+		});
 	});
 }
-// Allcollection Slider
-$(".all-collections__slider").slick({
-	dots: false,
-	infinite: false,
-	speed: 300,
-	slidesToShow: 1,
-	slidesToScroll: 1,
-	responsive: [
-		{
-			breakpoint: 1024,
-			settings: {
-				slidesToShow: 2.25,
-				slidesToScroll: 1,
-				infinite: true,
-				dots: false,
-			},
-		},
-		{
-			breakpoint: 600,
-			settings: {
-				slidesToShow: 2.25,
-				slidesToScroll: 1,
-			},
-		},
-		{
-			breakpoint: 480,
-			settings: {
-				slidesToShow: 2.25,
-				slidesToScroll: 1,
-			},
-		},
-	],
-});
-
 // Video slider----------
-$(".video__content").slick({
+$(".video__content-mobile").slick({
 	dots: false,
 	infinite: false,
 	speed: 300,
-	slidesToShow: 4,
+	slidesToShow: 2.5,
 	slidesToScroll: 1,
 	responsive: [
 		{
 			breakpoint: 1024,
 			settings: {
-				slidesToShow: 4,
+				slidesToShow: 2.5,
 				slidesToScroll: 1,
 				infinite: true,
 				dots: false,
@@ -98,7 +98,7 @@ $(".video__content").slick({
 		{
 			breakpoint: 600,
 			settings: {
-				slidesToShow: 1,
+				slidesToShow: 2.5,
 				slidesToScroll: 1,
 			},
 		},
